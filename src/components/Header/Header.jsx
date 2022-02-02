38829fe8772c732a2889d0a7a4834fe8efa4b05b
Eloi Magnien <eloi.magnien@gmail.com>
2022-02-02 09:30:34 +0100

    install redux

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,32 @@
-import { useState, useEffect } from "react";
+import './Header.css'
+import { useState } from "react";
 
 const Header = () => {
   const [isEditing, setIsEditing] = useState(false);
-  
+    console.log(isEditing);
+    const toggleEdit = () => {
+      setIsEditing(!isEditing)
+    }
   return (
     <div className="header">
     <h1>Welcome back<br />Tony Jarvis!</h1>
-    <button className="edit-button">Edit Name</button>
-    {/* <form className={isEditing ? "editName" : "editName hidden"}>
-    <div className="input-wrapper">
-        <label htmlFor="firstname"></label>
-        <input type="text" id="firstname" />
+    <button className={isEditing ? "edit-button hidden" : "edit-button"} onClick={toggleEdit}>Edit Name</button>
+    <form className={isEditing ? "editName" : "editName hidden"}>
+      <div className='editName__inputs '>
+        <div className="input-wrapper editName__input">
+          <label htmlFor="firstname"></label>
+          <input placeholder='Tony' type="text" id="firstname" />
+        </div>
+        <div className="input-wrapper editName__input">
+          <label htmlFor="lastname"></label>
+          <input placeholder='Jarvis' type="text" id="lastname" />
+        </div>
       </div>
-      <div className="input-wrapper">
-        <label htmlFor="lastname"></label>
-        <input type="text" id="lastname" />
+      <div className='editName__buttons'>
+        <button className="editName__btn edit-button save">Save</button>
+        <button className="editName__btn edit-button cancel" onClick={toggleEdit}>Cancel</button> 
       </div>
-      <button className="btn-save">Save</button>
-      <button className="btn-cancel" onClick={setIsEditing(false)}>Cancel</button>
-    </form> */}
+    </form>
   </div>
   );
 }
